180d16eaceefd56defaeedede8f42bf99206ff1d
prashanth-g <prashanthcsgce@gmail.com>
2018-09-25 23:39:42 +0530

    Added ApiInfo

--- src/main/java/com/prashanth/spring/swagger/docs/SwaggerDocsConfig.java
+++ src/main/java/com/prashanth/spring/swagger/docs/SwaggerDocsConfig.java
@@ -5,9 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -20,6 +24,19 @@ public class SwaggerDocsConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "TITLE",
+                "DESCIPRION",
+                "VERSION",
+                "TERMS OF SERVICE URL",
+                new Contact("NAME","URL","EMAIL"),
+                "LICENSE",
+                "LICENSE URL",
+                Collections.emptyList()
+        );
     }
 
 }
